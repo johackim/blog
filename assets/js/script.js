810fafcfbe35562478fe3eaebbcf713903640933
johackim <contact@johackim.com>
2020-06-29 16:27:04 +0200

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,62 @@
+import '../css/screen.css';
+
+const images = document.querySelectorAll('.kg-gallery-image img');
+
+images.forEach((image) => {
+    const container = image.closest('.kg-gallery-image');
+    const width = image.attributes.width.value;
+    const height = image.attributes.height.value;
+    const ratio = width / height;
+    container.style.flex = `${ratio} 1 0%`;
+});
+
+/* Subscribe */
+
+const subscribeButtons = document.querySelectorAll('.subscribe');
+const modal = document.querySelector('.modal');
+const closeModalBtn = document.querySelector('.modal .close');
+
+closeModalBtn.addEventListener('click', () => {
+    modal.classList.add('invisible');
+    window.history.pushState("", "", '/');
+});
+
+[].forEach.call(subscribeButtons, (button) => {
+    button.addEventListener('click', () => {
+        modal.classList.remove('invisible');
+    });
+});
+
+document.onkeydown = (evt) => {
+    evt = evt || window.event;
+    if (evt.keyCode === 27) {
+        modal.classList.add('invisible');
+    }
+};
+
+/* Notification */
+
+const notification = document.querySelector('.notification');
+const closeNotificationBtn = document.querySelector('.notification .close');
+
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const action = urlParams.get('action')
+
+if (action === 'subscribe') {
+    notification.classList.remove('hidden');
+}
+
+closeNotificationBtn.addEventListener('click', () => {
+    notification.classList.add('hidden');
+    window.history.pushState("", "", '/');
+});
+
+/* Custom */
+
+fetch('https://store.zapier.com/api/records?secret=GodoRihdRuwd1~')
+    .then((d) => d.json().then(({ subscribers }) => [...document.querySelectorAll('.subscribers')].forEach((span) => {
+        span.innerHTML = parseInt(subscribers, 10);
+    })));
+
+/* End custom */
